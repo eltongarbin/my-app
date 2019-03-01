6133372d03e218a4a4bc09c3cebc582f7340c289
Elton Garbin <eltongarbin@gmail.com>
2019-02-28 21:33:24 -0300

    provide dynamic arguments with graphql variabels

--- client/src/Recipes.jsx
+++ client/src/Recipes.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const recipesQuery = gql`
-  {
-    recipes {
+  query recipes($vegetarian: Boolean!) {
+    recipes(vegetarian: $vegetarian) {
       id
       title
     }
   }
 `;
 
-const Recipes = () => (
-  <Query query={recipesQuery}>
-    {({ data, loading, error }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <p>Something went wrong</p>;
+const Recipes = () => {
+  const [isVegetarian, setVegetarian] = useState(false);
+  function updateVegetarian({ target: { checked } }) {
+    setVegetarian(checked);
+  }
 
-      return (
-        <ul>
-          {data.recipes.map(({ id, title }) => (
-            <li key={id}>{title}</li>
-          ))}
-        </ul>
-      );
-    }}
-  </Query>
-);
+  return (
+    <Fragment>
+      <label>
+        <input
+          type="checkbox"
+          checked={isVegetarian}
+          onChange={updateVegetarian}
+        />
+        <span>vegetarian</span>
+      </label>
+      <Query query={recipesQuery} variables={{ vegetarian: isVegetarian }}>
+        {({ data, loading, error }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Something went wrong</p>;
+
+          return (
+            <ul>
+              {data.recipes.map(({ id, title }) => (
+                <li key={id}>{title}</li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
+    </Fragment>
+  );
+};
 
 export default Recipes;
