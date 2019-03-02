426ceb1e1f64c8efa4f10ba54885a0fc4f79a989
Elton Garbin <eltongarbin@gmail.com>
2019-03-01 23:09:13 -0300

    refetch manually or on timed intervals

--- client/src/Recipes.jsx
+++ client/src/Recipes.jsx
@@ -30,53 +30,56 @@ const Recipes = () => {
       <Query
         query={recipesQuery}
         variables={{ vegetarian }}
-        pollInterval={3000}
+        pollInterval={3500}
       >
         {({ data, loading, error, refetch }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Something went wrong</p>;
 
           return (
-            <ul>
-              {data.recipes.map(({ id, title, isStarred }) => (
-                <li key={id}>
-                  {title}
-                  <Mutation
-                    mutation={updateRecipeStarredMutation}
-                    refetchQueries={[
-                      {
-                        query: recipesQuery,
-                        variables: { vegetarian: false }
-                      },
-                      { query: recipesQuery, variables: { vegetarian: true } }
-                    ]}
-                    awaitRefetchQueries
-                  > 
-                    {(updateRecipeStarred, { loading, error }) => (
-                      <button
-                        onClick={() =>
-                          updateRecipeStarred({
-                            variables: {
-                              id,
-                              isStarred: !isStarred
-                            }
-                          })
-                        }
-                        className="star-btn"
-                        style={{
-                          color: isStarred ? 'orange' : 'grey',
-                          animation: loading
-                            ? 'inflate 0.7s ease infinite alternate'
-                            : 'none'
-                        }}
-                      >
-                        ★ {error && 'Failed to update'}
-                      </button>
-                    )}
-                  </Mutation>
-                </li>
-              ))}
-            </ul>
+            <Fragment>
+              <ul>
+                {data.recipes.map(({ id, title, isStarred }) => (
+                  <li key={id}>
+                    {title}
+                    <Mutation
+                      mutation={updateRecipeStarredMutation}
+                      refetchQueries={[
+                        {
+                          query: recipesQuery,
+                          variables: { vegetarian: false }
+                        },
+                        { query: recipesQuery, variables: { vegetarian: true } }
+                      ]}
+                      awaitRefetchQueries
+                    >
+                      {(updateRecipeStarred, { loading, error }) => (
+                        <button
+                          onClick={() =>
+                            updateRecipeStarred({
+                              variables: {
+                                id,
+                                isStarred: !isStarred
+                              }
+                            })
+                          }
+                          className="star-btn"
+                          style={{
+                            color: isStarred ? 'orange' : 'grey',
+                            animation: loading
+                              ? 'inflate 0.7s ease infinite alternate'
+                              : 'none'
+                          }}
+                        >
+                          ★ {error && 'Failed to update'}
+                        </button>
+                      )}
+                    </Mutation>
+                  </li>
+                ))}
+              </ul>
+              <button onClick={() => refetch()}>Refresh Recipes</button>
+            </Fragment>
           );
         }}
       </Query>
